5681ffe4503ee9f6d329c39f771c49b9399ba000
miyawaki <121045410+m-miyawaki-m@users.noreply.github.com>
2025-01-15 15:06:35 +0900

    asciidoc.m の更新

--- note/asciidoc.m
+++ note/asciidoc.m
@@ -118,3 +118,117 @@ Asciidocのコード例:
 文字数: 898
 トークン数: 124
 
+
+Bootstrapを使いながら、ページの内容をAsciidocの内容で差し込むには、以下のように設定を行います。
+
+
+---
+
+手順
+
+1. AsciidocにBootstrapのCSS/JSを読み込む Asciidocに直接Bootstrapのリンクを埋め込むことで、Bootstrapのスタイルを利用可能にします。
+
+
+2. Asciidocの内容をHTMLタブに差し込む Asciidocの構文を使って、タブごとに内容を記述し、HTML出力時にBootstrapのタブ機能を適用します。
+
+
+
+
+---
+
+実装例
+
+以下は、Bootstrapのタブ機能を使用し、Asciidocの内容を差し込む例です。
+
+Asciidocのコード例
+
+++++
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+++++
+
+=== タブ管理のサンプル
+
+以下にタブを作成し、各タブに内容を差し込みます。
+
+++++
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="page1-tab" data-bs-toggle="tab" data-bs-target="#page1" type="button" role="tab">Page 1</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="page2-tab" data-bs-toggle="tab" data-bs-target="#page2" type="button" role="tab">Page 2</button>
+  </li>
+</ul>
+<div class="tab-content">
+  <div class="tab-pane fade show active" id="page1" role="tabpanel">
+++++
+
+.Page 1の内容
+Asciidocの内容をここに記述できます。
+例えば、以下のリストなど:
+* アイテム1
+* アイテム2
+
+++++
+  </div>
+  <div class="tab-pane fade" id="page2" role="tabpanel">
+++++
+
+.Page 2の内容
+表を記述することも可能です。
+
+[cols="1,1", options="header"]
+|===
+|列1 |列2
+|データ1 |データ2
+|データ3 |データ4
+|===
+
+++++
+  </div>
+</div>
+++++
+
+
+---
+
+仕組みのポイント
+
+1. Bootstrapのタブ構造 Bootstrapが提供するタブ機能の構造をHTML内に記述します。nav-tabsやtab-contentを活用します。
+
+
+2. Asciidocの内容をタブごとに差し込む 各タブの<div>内にAsciidocで記述した内容を埋め込みます。
+
+
+3. HTML埋め込み (++++ブロック) ++++ブロックでHTMLを直接記述し、その中にAsciidocの構文を含めて利用します。
+
+
+
+
+---
+
+出力イメージ
+
+1. Page 1: リスト形式の内容が表示される。
+
+
+2. Page 2: 表形式のデータが表示される。
+
+
+
+
+---
+
+補足
+
+BootstrapのリンクはCDN経由で読み込んでいますが、必要に応じてローカルにファイルを配置して使用することも可能です。
+
+data-bs-toggleやtab-paneのクラスを変更することで、Bootstrapのスタイルに応じたカスタマイズが可能です。
+
+
+
+---
+
+この方法で、Asciidocの内容を直接タブに差し込みつつ、Bootstrapのスタイルを活用できます。
+
